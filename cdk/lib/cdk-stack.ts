e5cd5ed9f40559d65b94708929d48ccd0a289076
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-21 23:39:57 +0900

    Bumps node modules

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -128,6 +128,18 @@ export class CloudfrontCdnTemplateStack extends cdk.Stack {
               }),
             ],
           }),
+          'bedrock-policy': new iam.PolicyDocument({
+            statements: [
+              new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                  'bedrock:InvokeModel*',
+                  'logs:PutLogEvents',
+                ],
+                resources: ['*'],
+              }),
+            ],
+          }),
         },
       }),
       loggingFormat: lambda.LoggingFormat.JSON,
