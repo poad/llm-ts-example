3368f5d9a074a00e87b3480dd85df926a755c76c
Kenji Saito <ken-yo@mbr.nifty.com>
2024-11-26 12:45:22 +0900

    Bumps node modules

--- cdk/lambda/llm.ts
+++ cdk/lambda/llm.ts
@@ -6,7 +6,8 @@ import { BaseChatModel } from '@langchain/core/language_models/chat_models';
 
 export function selectLlm(modelType?: string): {
   platform: 'aws' | 'azure' | 'anthropic';
-  model: BaseChatModel,
+  modelName: string;
+  model: BaseChatModel;
 } {
   const bedrockRegion = process.env.BEDROCK_AWS_REGION;
   const credentials = process.env.AWS_SECRET_ACCESS_KEY && process.env.AWS_ACCESS_KEY_ID ? {
@@ -19,6 +20,7 @@ export function selectLlm(modelType?: string): {
     logger.debug('use: cohere.command-r-plus-v1:0 on AWS Bedrock');
     return {
       platform: 'aws',
+      modelName: 'cohere.command-r-v1:0',
       model: new ChatBedrockConverse({
         model: 'cohere.command-r-v1:0',
         temperature: 0,
@@ -35,6 +37,7 @@ export function selectLlm(modelType?: string): {
     logger.debug('use: Meta LLama 3.2 1B Instruct');
     return {
       platform: 'aws',
+      modelName: 'us.meta.llama3-2-1b-instruct-v1:0',
       model: new ChatBedrockConverse({
         model: 'us.meta.llama3-2-1b-instruct-v1:0',
         temperature: 0,
@@ -51,6 +54,7 @@ export function selectLlm(modelType?: string): {
     logger.debug('use: Meta LLama 3.2 3B Instruct');
     return {
       platform: 'aws',
+      modelName: 'us.meta.llama3-2-3b-instruct-v1:0',
       model: new ChatBedrockConverse({
         model: 'us.meta.llama3-2-3b-instruct-v1:0',
         temperature: 0,
@@ -67,6 +71,7 @@ export function selectLlm(modelType?: string): {
     logger.debug('use: Anthropic Claude 3.5 Sonnet');
     return {
       platform: 'anthropic',
+      modelName: process.env.CLAUDE_MODEL ?? 'claude-3-5-sonnet-20240620',
       model: new ChatAnthropic({
         model: process.env.CLAUDE_MODEL ?? 'claude-3-5-sonnet-20240620',
         streaming: true,
@@ -81,6 +86,7 @@ export function selectLlm(modelType?: string): {
     logger.debug('use: GPT-4o mini on Azure OpenAI Service');
     return {
       platform: 'azure',
+      modelName: 'gpt-4o-mini',
       model: new AzureChatOpenAI({
         modelName: 'gpt-4o-mini',
         temperature: 0,
@@ -94,6 +100,7 @@ export function selectLlm(modelType?: string): {
   logger.debug('use: GPT-4o on Azure OpenAI Service');
   return {
     platform: 'azure',
+    modelName: 'gpt-4o',
     model: new AzureChatOpenAI({
       modelName: 'gpt-4o',
       temperature: 0,
