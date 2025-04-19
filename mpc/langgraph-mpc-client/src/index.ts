a01bf55b815c564c6d84374e8f5a02d736e91bf3
Kenji Saito <ken-yo@mbr.nifty.com>
2025-04-19 18:43:20 +0900

    Add the MCP Client by LangGraph.js

---
+++ mpc/langgraph-mpc-client/src/index.ts
@@ -0,0 +1,23 @@
+import { input } from "@inquirer/prompts";
+import dotenv from "dotenv";
+import * as sourceMap from 'source-map-support';
+import { createAgent } from "./agent.js";
+sourceMap.install();
+
+dotenv.config();
+
+try {
+  const agent = await createAgent();
+  try {
+    const content = await input({ message: '' });
+    if (content && content !== 'exit' && content !== 'quit') {
+      const agentResponse = await agent.invoke(content);
+      console.log(agentResponse.messages.reverse()[0].content);
+    }
+  } finally {
+    // Clean up connection
+    await agent.close();
+  }
+} catch (e) {
+  console.error(e);
+}
