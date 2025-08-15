84907d79b5a7fb4265916dfc51dd6b1dbe4b2466
Kenji Saito <ken-yo@mbr.nifty.com>
2025-08-15 16:31:10 +0900

    Fix the build errors

--- agents/agent-mastra/src/mastra/index.ts
+++ agents/agent-mastra/src/mastra/index.ts
@@ -1,12 +1,17 @@
 
 import { Mastra } from '@mastra/core/mastra';
-import { ConsoleLogger } from '@mastra/core/logger';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
 import { weatherAgent } from './agents';
 import { LangfuseExporter } from 'langfuse-vercel';
 
 export const mastra = new Mastra({
   agents: { weatherAgent },
-  logger: new ConsoleLogger({
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ':memory:',
+  }),
+  logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
   }),
--- update.sh
+++ update.sh
@@ -13,68 +13,7 @@ if [ $result -ne 0 ]; then
   exit $result
 fi
 
-cd "${CURRENT}/basic" || exit
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
-fi
-echo ""
-pwd
-npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm build
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
-fi
-
-cd "${CURRENT}/rag" || exit
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
-fi
-echo ""
-pwd
-npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm build
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
-fi
-
 cd "${CURRENT}/agents/agent-mastra" || exit
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
-fi
-echo ""
-pwd
-npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm audit --fix && pnpm up -r && pnpm build
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
-fi
-
-cd "${CURRENT}/node-llama-cpp" || exit
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
-fi
-echo ""
-pwd
-npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm build
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
-fi
-
-
-cd "${CURRENT}/webllm" || exit
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
