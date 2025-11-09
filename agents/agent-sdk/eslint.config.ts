6ba3fe209d7444f3cfcf3aca85729cf3cfe89d1c
Kenji Saito <1867845+poad@users.noreply.github.com>
2025-11-09 15:24:23 +0900

    Add the Claude Agent SDK Sample (#139)

---
+++ agents/agent-sdk/eslint.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from 'typescript-eslint';
+import eslintImport from 'eslint-plugin-import';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, './.gitignore');
+
+export default defineConfig(
+  includeIgnoreFile(gitignorePath),
+  {
+    ignores: [
+      '**/*.d.ts',
+      '*.{js,jsx}',
+      'src/tsconfig.json',
+      'src/stories',
+      '**/*.css',
+      'node_modules/**/*',
+      './.next/*',
+      'out',
+      '.storybook',
+      'dist',
+      '.vinxi',
+      '.output',
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+        project: [
+          './tsconfig.json',
+          './tsconfig-eslint.json',
+        ],
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    extends: [eslintImport.flatConfigs.recommended, eslintImport.flatConfigs.typescript],
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/quotes': ['error', 'single'],
+      'import/no-unresolved': 'off',
+    },
+  }
+);
