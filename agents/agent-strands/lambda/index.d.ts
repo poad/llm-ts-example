3db60dac8c06731c422e322a78691cd462f9c862
Kenji Saito <ken-yo@mbr.nifty.com>
2025-12-06 19:52:55 +0900

    Bumps node modules

--- agents/agent-strands/lambda/index.d.ts
+++ agents/agent-strands/lambda/index.d.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
-export declare const handle: ({ question: message, model: model }: {
-    question: string;
+export declare const handle: ({ message: message, model: model }: {
+    message: string;
     model: string;
 }, output: NodeJS.WritableStream) => Promise<void>;
 export declare const handler: import("aws-lambda").StreamifyHandler<APIGatewayProxyEvent, void>;
