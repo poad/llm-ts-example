3753dd671f706e7387e88efc7d2a9f621c72641d
Kenji Saito <ken-yo@mbr.nifty.com>
2026-01-15 09:40:44 +0900

    Bumps node modules

--- basic/cdk/lambda/app.ts
+++ basic/cdk/lambda/app.ts
@@ -1,15 +1,8 @@
-import { ChatPromptTemplate } from '@langchain/core/prompts';
-import { StringOutputParser } from '@langchain/core/output_parsers';
 import { LanguageModelLike } from '@langchain/core/language_models/base';
 import { BaseChatModel } from '@langchain/core/language_models/chat_models';
 import { createAgent } from 'langchain';
 
-export const createApp = ({ model, modelName }: { model: LanguageModelLike & BaseChatModel, modelName: string }) => {
-  const qaPrpmpt = `Answer the user's question to the best of your ability.
-    However, please keep your answers brief and in the same language as the question.
-
-    {question}`;
-
+export const createApp = ({ model }: { model: LanguageModelLike & BaseChatModel, modelName: string }) => {
   const agent = createAgent({
     model,
     tools: [],
@@ -20,4 +13,4 @@ export const createApp = ({ model, modelName }: { model: LanguageModelLike & Bas
   });
 
   return agent;
-}
+};
