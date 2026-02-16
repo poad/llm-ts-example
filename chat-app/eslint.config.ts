11724ed93abc7f75e2fd4fdfdddec6b4774a271a
Kenji Saito <ken-yo@mbr.nifty.com>
2026-02-17 05:19:16 +0900

    Bumps node modules

--- chat-app/eslint.config.ts
+++ chat-app/eslint.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
-import { configs, parser } from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 // @ts-expect-error ignore plugin type
 import pluginPromise from 'eslint-plugin-promise';
+// @ts-expect-error ignore type error
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import globals from 'globals';
+import { configs, parser } from 'typescript-eslint';
 
 import { includeIgnoreFile } from '@eslint/compat';
 import path from 'node:path';
@@ -13,7 +18,7 @@ import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, '.gitignore');
+const gitignorePath = path.resolve(__dirname, './.gitignore');
 
 
 const eslintConfig = defineConfig([
@@ -28,34 +33,45 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
   ]),
+  includeIgnoreFile(gitignorePath),
+  {
+    ignores: [
+      '**/*.d.ts',
+      '*.js',
+      'src/tsconfig.json',
+      'src/next-env.d.ts',
+      'src/stories',
+      'node_modules/**/*',
+      './.next/*',
+    ],
+  },
   eslint.configs.recommended,
   ...configs.strict,
   ...configs.stylistic,
   pluginPromise.configs['flat/recommended'],
+  reactHooks.configs.flat.recommended,
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat['jsx-runtime'],
+    ...jsxA11yPlugin.flatConfigs.recommended,
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ...react.configs.flat.recommended.languageOptions,
+      ...jsxA11yPlugin.flatConfigs.recommended.languageOptions,
       parser,
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        allowDefaultProject: ['*.ts', '*.js'],
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
       },
     },
-
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-    ],
     plugins: {
       '@stylistic': stylistic,
     },
     rules: {
-      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/semi': 'error',
       '@stylistic/indent': ['error', 2],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/arrow-parens': ['error', 'always'],
       '@stylistic/quotes': ['error', 'single'],
     },
   },
